a73d10fcbd7b9a527ef3ae53a82adfaa647d1a89
Stéphane Bazzé <zarakeye@users.noreply.github.com>
2024-12-22 09:47:39 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,13 +4,13 @@ const cors = require('cors')
 const router = require('./routes')
 
 const app = express()
-app.use(cors(
+app.use(cors({
   origin: [
     /^http:\/\/localhost:\d+$/,
     'https://sport-see-frontend.vercel.app',
   ],
   credentials: true
-))
+}))
 const port = 3000
 
 app.use(router)
